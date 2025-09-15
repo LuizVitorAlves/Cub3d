737fbc92021360704c3a7c7c963377046e6c6a9c
LuizVitorAlves <l.v.alvesjr@gmail.com>
2025-09-15 09:40:38 -0300

    Finalização da lógica do tokenizador

--- tokenizador.c
+++ tokenizador.c
@@ -6,7 +6,7 @@
 /*   By: lalves-d <lalves-d@student.42rio>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 08:44:15 by lalves-d          #+#    #+#             */
-/*   Updated: 2025/09/15 08:48:01 by lalves-d         ###   ########.fr       */
+/*   Updated: 2025/09/15 09:40:00 by lalves-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,36 +26,40 @@ static int is_delim(char c, const char *delim)
     return 0;
 }
 
-char *ft_strtok(char *str, const char *delim)
+static char	*init_ptr(char *str, const char *delim, char **save_ptr)
 {
-    static char *save_ptr = NULL;
-    char *ptr;
-    char *token_start;
+	if (!delim)
+		return (NULL);
+	if (str)
+		return (str);
+	return (*save_ptr);
+}
 
-    if (delim == NULL)
-        return NULL;
-    if (str != NULL)
-        ptr = str;
-    else
-        ptr = save_ptr;
-    if (ptr == NULL)
-        return NULL;
-    while (*ptr && is_delim(*ptr, delim))
-        ptr++;
-    if (*ptr == '\0')
-    {
-        save_ptr = NULL;
-        return NULL;
-    }
-    token_start = ptr;
-    while (*ptr && !is_delim(*ptr, delim))
-        ptr++;
-    if (*ptr != '\0')
-    {
-        *ptr = '\0';
-        save_ptr = ptr + 1;
-    }
-    else
-        save_ptr = NULL;
-    return token_start;
+char	*ft_strtok(char *str, const char *delim)
+{
+	static char	*save_ptr = NULL;
+	char		*ptr;
+	char		*token_start;
+
+	ptr = init_ptr(str, delim, &save_ptr);
+	if (!ptr)
+		return (NULL);
+	while (*ptr && is_delim(*ptr, delim))
+		ptr++;
+	if (*ptr == '\0')
+	{
+		save_ptr = NULL;
+		return (NULL);
+	}
+	token_start = ptr;
+	while (*ptr && !is_delim(*ptr, delim))
+		ptr++;
+	if (*ptr != '\0')
+	{
+		*ptr = '\0';
+		save_ptr = ptr + 1;
+	}
+	else
+		save_ptr = NULL;
+	return (token_start);
 }
